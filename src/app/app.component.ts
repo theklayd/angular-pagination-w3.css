caffbbca7802b4a11cb817dd16953632e0afb9c4
theklayd <clydesantos1028@gmail.com>
2019-01-21 18:10:03 +0800

    xxxx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-pagination-w3css';
 
   dataSize:number = 350
   dataPerPage:number
